d89458852d1a244f91e9342f05f8d133238a3f9c
Jasmine Andresol <jasmineandresol@Jasmines-Air.lan>
2023-11-22 22:17:37 -0500

    matching algorithm

--- match.py
+++ match.py
@@ -4,11 +4,20 @@ import pandas as pd
 csv_path = "college_data.csv"
 df = pd.read_csv(csv_path)
 
+#column names
+columns = {
+    'Region': 'HD2021.Bureau of Economic Analysis (BEA) regions',
+    'State': 'HD2021.FIPS state code',
+    'Enrollment': 'DRVEF2021.Full-time undergraduate enrollment',
+    'SAT Reading and Writing': 'ADM2021_RV.SAT Evidence-Based Reading and Writing 75th percentile score',  
+    'SAT Math': 'ADM2021_RV.SAT Math 75th percentile score'
+}
+
 # User preferences (example values, replace with user input)
 user_preferences = {
-    'BEA Region': 'New England (CT, ME, MA, NH, RI, VT)',
+    'Region': 'New England (CT, ME, MA, NH, RI, VT)',
     'State': 'Massachussets',
-    'Enrollment Size': 8000,
+    'Enrollment': 8000,
     'SAT Reading and Writing': 800,
     'SAT Math': 800
 }
@@ -19,39 +28,33 @@ df = df.dropna(subset=['DRVEF2021.Full-time undergraduate enrollment'])
 # Filter rows where 'HD2021.Institutional category' matches the specified value
 df = df[df['HD2021.Institutional category'] == 'Degree-granting, primarily baccalaureate or above']
 
-# Define a function to calculate matching score for each criterion
-def calculate_score(value, user_preference, weight):
-    # Adjusted weighting algorithm to assign scores based on preferences
-    if user_preference == value:
-        return 100 * weight
-    else:
-        # Adjusted weighting algorithm for enrollment size using a linear approach
-        if user_preference == 'Enrollment Size':
-            try:
-                value = float(value)
-            except ValueError:
-                return 0  # Assign a score of 0 for non-numeric values
-            
-            # Calculate enrollment score using a linear approach
-            enrollment_score = max(0, 100 - 10 * abs(value - user_preferences['Enrollment Size']) / user_preferences['Enrollment Size'])
-            return enrollment_score * weight
-        elif user_preference == 'SAT Score':
-            try:
-                value = float(value)
-            except ValueError:
-                return 0  # Assign a score of 0 for non-numeric values
-            
-            # Calculate SAT score using a linear approach
-            sat_score = max(0, 100 - abs(value - user_preferences['SAT Reading and Writing']) - abs(value - user_preferences['SAT Math']))
-            return sat_score * weight
-        else:
-            return 0  # For other preferences, assign a score of 0
+# Define three separate functions for each preference
+def string_match(value, user_preference, weight):
+    return 100 * weight if user_preference == value else 0
+
+def score_enrollment_size(value, weight):
+    try:
+        value = float(value)
+    except ValueError:
+        return 0
+
+    enrollment_score = max(0, 100 - 10 * abs(value - user_preferences['Enrollment']) / user_preferences['Enrollment'])
+    return enrollment_score * weight
+
+def score_sat(value, weight):
+    try:
+        value = float(value)
+    except ValueError:
+        return 0
+
+    sat_score = max(0, 100 - abs(value - user_preferences['SAT Reading and Writing']) - abs(value - user_preferences['SAT Math']))
+    return sat_score * weight
 
 # Calculate scores for each criterion in the DataFrame
-df['BEA_Region_Score'] = df['HD2021.Bureau of Economic Analysis (BEA) regions'].apply(lambda x: calculate_score(x, user_preferences['BEA Region'], 1))
-df['State_Score'] = df['HD2021.FIPS state code'].apply(lambda x: calculate_score(x, user_preferences['State'], 1))
-df['Enrollment_Score'] = df['DRVEF2021.Full-time undergraduate enrollment'].apply(lambda x: calculate_score(x, 'Enrollment Size', 1))
-df['SAT_Score'] = df[['ADM2021_RV.SAT Evidence-Based Reading and Writing 75th percentile score', 'ADM2021_RV.SAT Math 75th percentile score']].mean(axis=1).apply(lambda x: calculate_score(x, 'SAT Score', 1))
+df['BEA_Region_Score'] = df[columns['Region']].apply(lambda x: string_match(x, user_preferences['Region'], 1))
+df['State_Score'] = df[columns['State']].apply(lambda x: string_match(x, user_preferences['State'], 1))
+df['Enrollment_Score'] = df[columns['Enrollment']].apply(lambda x: score_enrollment_size(x, 1))
+df['SAT_Score'] = df[['ADM2021_RV.SAT Evidence-Based Reading and Writing 75th percentile score', 'ADM2021_RV.SAT Math 75th percentile score']].mean(axis=1).apply(lambda x: score_sat(x, 1))
 
 # Calculate the total score for the DataFrame
 total_weight = df[['BEA_Region_Score', 'State_Score', 'Enrollment_Score', 'SAT_Score']].sum(axis=1)
