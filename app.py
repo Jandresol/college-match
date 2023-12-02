2fd649974fcbecbef369781ae41e0429f30c29a8
Jasmine Andresol <jasmineandresol@Jasmines-Air.lan>
2023-12-02 04:03:17 -0500

    commit

--- app.py
+++ app.py
@@ -1,52 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from match import load_data, clean_data, calculate_total_match
+from constants import REGION_COLUMN, STATE_COLUMN, questions, form_fields, weights
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
-
-# CSV Columns
-CSV_PATH = "college_data.csv"
-SIZE_COLUMN = 'HD2021.Institution size category'
-INSTITUTION_CATEGORY_COLUMN = 'HD2021.Institutional category'
-REGION_COLUMN = 'HD2021.Bureau of Economic Analysis (BEA) regions'
-STATE_COLUMN = 'HD2021.FIPS state code'
-CONTROL_COLUMN = 'HD2021.Control of institution'
-GRADUATION_RATE_COLUMN = 'DRVGR2021_RV.Graduation rate, total cohort'
-ADMISSIONS_COLUMN = 'DRVADM2021_RV.Percent admitted - total'
-URBANIZATION_COLUMN = 'HD2021.Degree of urbanization (Urban-centric locale)'
-NET_PRICE_PUBLIC_COLUMN = 'SFA2021_RV.Average net price-students awarded grant or scholarship aid, 2020-21 Public'
-NET_PRICE_PRIVATE_COLUMN = 'SFA2021_RV.Average net price-students awarded grant or scholarship aid, 2020-21 Private'
-NET_PRICE_COLUMN = 'Combined Net Price'
-
-# Step Questions
-questions = [
-    'Select Region',
-    'Select State',
-    'Select Size',
-    'Select Control',
-    'Enter Graduation Rate',
-    'Enter Admissions Rate',
-    'Select Urbanization',
-    'Enter Net Price'
-]
-
-# Step Form Fields
-form_fields = [
-    REGION_COLUMN, STATE_COLUMN, SIZE_COLUMN, CONTROL_COLUMN,
-    GRADUATION_RATE_COLUMN, ADMISSIONS_COLUMN, URBANIZATION_COLUMN, NET_PRICE_COLUMN
-]
-
-# Define weights for each criterion
-weights = {
-    REGION_COLUMN: 1,
-    STATE_COLUMN: 1,
-    SIZE_COLUMN: 1,
-    CONTROL_COLUMN: 1,
-    GRADUATION_RATE_COLUMN: 1,
-    ADMISSIONS_COLUMN: 1,
-    URBANIZATION_COLUMN: 1,
-    NET_PRICE_COLUMN: 1,
-}
 
 # Load and clean data
 df = load_data("college_data.csv")
@@ -96,7 +53,9 @@ def results():
     sorted_data = df.sort_values(by='Percent_Match', ascending=False)
     top_ten_data = sorted_data.head(10)
     results_html = top_ten_data[['institution name', 'Percent_Match']].to_html(index=False)
-    return render_template('results.html', results_html=results_html, region_values=region_values, state_values=state_values, form_fields=form_fields, questions=questions)
+    
+    # Pass top_ten_data to the template
+    return render_template('results.html', results_html=results_html, top_ten_data=top_ten_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
